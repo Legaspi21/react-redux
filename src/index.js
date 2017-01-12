0a1e4323e24115e690100b0f50fb52b12ba53ee8
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-12 00:24:33 -0800

    Video selection

--- src/index.js
+++ src/index.js
@@ -11,9 +11,15 @@ class App extends Component {
 		constructor(props) {
 				super(props);
 
-				this.state = { videos: [] };
+				this.state = {
+						videos: [],
+						selectedVideo: null
+				};
 				YTSearch({key: API_KEY, term: 'fetty slaps'}, (videos) => {
-						this.setState({ videos });
+					this.setState({
+						videos: videos,
+						selectedVideo: videos[0]
+					});
 				})
 
 		}
@@ -22,8 +28,10 @@ class App extends Component {
 			return ( 
 				<div>
 					<SearchBar />
-					<VideoDetail video={this.state.videos[0]}/>
-					<VideoList videos={this.state.videos}/>
+					<VideoDetail video={this.state.selectedVideo}/>
+					<VideoList
+						onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+						videos={this.state.videos}/>
 				</div>
 			);
 		}
