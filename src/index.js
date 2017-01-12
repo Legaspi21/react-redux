0257ed2df93ae277457ddb5cd0f4ae7020801dcb
Aidan Legaspi <aidanlegaspi1@gmail.com>
2017-01-12 00:46:44 -0800

    Add video search

--- src/index.js
+++ src/index.js
@@ -15,19 +15,24 @@ class App extends Component {
 						videos: [],
 						selectedVideo: null
 				};
-				YTSearch({key: API_KEY, term: 'fetty slaps'}, (videos) => {
+				
+				this.videoSearch('fetty slaps');
+
+		}
+		
+		videoSearch(term) {
+				YTSearch({key: API_KEY, term: term}, (videos) => {
 					this.setState({
 						videos: videos,
 						selectedVideo: videos[0]
 					});
 				})
-
 		}
 
 		render() {    
 			return ( 
 				<div>
-					<SearchBar />
+					<SearchBar onSearchTermChange={term => this.videoSearch(term) }/>
 					<VideoDetail video={this.state.selectedVideo}/>
 					<VideoList
 						onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
